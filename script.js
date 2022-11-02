c40ab6747d26fbade9a716a834297350a3fdc97b
ManaSiva <105069041+ManaSiva@users.noreply.github.com>
2022-11-02 19:16:26 +0530

    anni files avvaali devudaa

---
+++ script.js
@@ -0,0 +1,21 @@
+// global.document = new JSDOM(html).window.document;
+     
+
+// var body = document.querySelector("div");
+//     document.querySelector("#red").addEventListener('click', function(){
+//         body.style.backgroundColor = 'red';
+//     });
+//     document.querySelector("#green").addEventListener('click', function(){
+//         body.style.backgroundColor = 'green';
+//     });
+//     document.querySelector("#blue").addEventListener('click', function(){
+//         body.style.backgroundColor = 'blue';
+//     });
+//     document.querySelector("#black").addEventListener('click', function(){
+//         body.style.backgroundColor = 'black';
+//     });
+  
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+//   }
